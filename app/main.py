963a2fe279b1c43a89caa4c9f2ebf34f893d30c0
Олександр <oleksandr@MacBook-Air-Oleksandr.local>
2025-08-11 16:23:30 -0500

    Solution

--- app/main.py
+++ app/main.py
@@ -2,12 +2,14 @@ import os
 
 import requests
 
+
 API_KEY = os.getenv("WEATHER_API_KEY")
 def get_weather() -> None:
-    url = f"https://api.weatherapi.com/v1/current.json?key={API_KEY}&q=Paris&aqi=no"
+    url = (
+        f"https://api.weatherapi.com/v1/current.json?key={API_KEY}&q=Paris&aqi=no"
+    )
     res = requests.get(url)
     print(res.json())
-
 
 
 if __name__ == "__main__":
